6d0f9a66e612c740ee16e74f5b1f71bdb6f16009
kost9klinov@gmail.com <kost9klinov@gmail.com>
2025-02-20 10:21:16 +0800

    [+] Added xcode init project

---
+++ Privacy Fortress/Privacy_FortressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Privacy_FortressApp.swift
+//  Privacy Fortress
+//
+//  Created by Константин Клинов on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Privacy_FortressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
